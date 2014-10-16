bb35bad81da0a0ea88511ce9645f26b5420b4229
Den_dp <Denis.Bendrikov@gmail.com>
2014-10-16 14:14:47 +0300

    working copy

--- client/SalesForceCasesViewModel.js
+++ client/SalesForceCasesViewModel.js
@@ -1,62 +1,87 @@
 function SalesForceCasesViewModel(config) {
     // Data
     var self = this;
-    self.folders = ['open', 'closed'];
-
     self.chosenCaseData = ko.observable();
     self.chosenCaseId = ko.observable();
     self.chosenCaseComments = ko.observable();
+    self.chosenCaseComments.isLoaded = ko.observable(false);
+
+    self.folders = ['open', 'closed'];
     self.chosenFolderId = ko.observable();
     self.chosenFolderData = ko.observableArray();
+    self.chosenFolderData.isLoaded = ko.observable(false);
 
     // Behaviours
     self.goToFolder = function (folder) {
         location.hash = folder;
     };
     self.goToCase = function (caseObject) {
-        location.hash = /*caseObject.folder*/  self.chosenFolderId() + '/' + caseObject.id
+        location.hash = self.chosenFolderId() + '/' + caseObject.id
     };
 
     // Client-side routes    
     Sammy(function () {
-        this.get('#:folder', function () {
-            self.chosenFolderId(this.params.folder);
+        this.disable_push_state = true;
 
-            $.get(config.getCasesUrl, { "closed": this.params.folder == 'closed' })
-                .done(function (data) {
-                    self.chosenFolderData(data);
-                })
-                //.always(function () {
-                //    self.cases.isLoaded(true);
-                //});
-        });
-
-        this.get('#:folder/:caseId', function () {
-
-            self.chosenCaseId(this.params.caseId);
-            self.chosenFolderData(this.params.folder);
-
-            $.get(config.getCaseDetailsUrl.replace(/\{\{caseId\}\}/, this.params.caseId))
-                .done(function(data){
-                    self.chosenCaseData(data);
-                })
-                //.always(function () {
-                //    self.chosenCaseData.isCaseCommentsLoaded(true);
-                //});
-
-            $.get(config.getCaseCommentsUrl.replace(/\{\{caseId\}\}/, this.params.caseId))
-                .done(function (data) {
-                    self.chosenCaseComments(data);
-                })
-                //.always(function () {
-                //    self.chosenCaseData.isCaseCommentsLoaded(true);
-                //});
-
-        });
-
+        // routes definition
+        this.get('#:folder', folderRoute);
+        this.get('#:folder/:caseId', caseCommentsRoute);
         this.get('', function () {
             this.app.runRoute('get', '#open');
         });
+
+        //routes implementation
+        function folderRoute() {
+            var routeParamFolderName = this.params.folder;
+
+            self.chosenFolderData.isLoaded(false);
+            self.chosenFolderId(routeParamFolderName);
+
+            return $.get(config.getCasesUrl, { "closed": routeParamFolderName == 'closed' })
+                .done(function (casesArray) {
+                    self.chosenFolderData(casesArray);
+                })
+                .always(function () {
+                    self.chosenFolderData.isLoaded(true);
+                });
+        }
+
+
+        function caseCommentsRoute() {
+            var routeParamCaseId = this.params.caseId;
+
+            self.chosenCaseId(routeParamCaseId);
+            self.chosenCaseComments.isLoaded(false);
+
+            if (!self.chosenFolderData.isLoaded()) {
+                // we need this step to resolve needed data from `folderRoute`
+                // this happens when we load `caseCommentsRoute` omitting `folderRoute`
+                // because SammyJS have no idea how routes can be related
+                folderRoute.bind(this)()
+                    .done(function () {
+
+                        $.get(config.getCaseDetailsUrl.replace(/\{\{caseId\}\}/, routeParamCaseId ))
+                            .done(function (caseDetails) {
+                                self.chosenCaseData(caseDetails);
+                            })
+                    });
+            } else {
+                // case when `chosenFolderData` data was prepared in `folderRoute` before we load `caseCommentsRoute` route.
+                $.get(config.getCaseDetailsUrl.replace(/\{\{caseId\}\}/, routeParamCaseId ))
+                    .done(function (caseDetails) {
+                        self.chosenCaseData(caseDetails);
+                    })
+            }
+
+            $.get(config.getCaseCommentsUrl.replace(/\{\{caseId\}\}/, routeParamCaseId ))
+                .done(function (commentsArray) {
+                    self.chosenCaseComments(commentsArray);
+                })
+                .always(function () {
+                    self.chosenCaseComments.isLoaded(true);
+                });
+
+        }
 
     }).run();
 
